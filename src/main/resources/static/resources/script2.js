df827322d574aa5e101a4a028bb005ec147624ae
alessandroscarlatti <violanotesnoextras@gmail.com>
2019-01-28 20:53:16 -0600

    add stopping power :)

--- src/main/resources/static/resources/script2.js
+++ src/main/resources/static/resources/script2.js
@@ -9,7 +9,8 @@ function TaskExecution(paramsObjOrConfig) {
     }
 
     if (config == null) {
-        config = function(){};
+        config = function () {
+        };
     }
 
     if (params == null) {
@@ -29,10 +30,13 @@ function TaskExecution(paramsObjOrConfig) {
     }
 
     function isFinished() {
-        return (this.context.status === "COMPLETED" || this.context.status === "FAILED" || this.context.status === "STOPPED");
+        return (context.status === "COMPLETED" || context.status === "FAILED" || context.status === "STOPPED");
     }
 
     function statusStarting() {
+        if (isFinished())
+            return;
+
         // only allow if task is ready.
         context.status = "STARTING";
         context.exitCode = "UNKNOWN";
@@ -40,20 +44,30 @@ function TaskExecution(paramsObjOrConfig) {
     }
 
     function statusStarted() {
+        if (isFinished())
+            return;
+
         context.status = "STARTED";
         updateSubscribers();
     }
 
     function statusStopping() {
+        if (isFinished())
+            return;
+
         context.status = "STOPPING";
         updateSubscribers();
     }
 
     function statusStopped(exitCode) {
+        if (isFinished())
+            return;
+
         if (exitCode == null)
             exitCode = "STOPPED";
 
-        context.status = exitCode;
+        context.status = "STOPPED";
+        context.exitCode = exitCode;
         updateSubscribers();
     }
 
@@ -62,6 +76,9 @@ function TaskExecution(paramsObjOrConfig) {
      * or an exit code object and a result object.
      */
     function statusCompleted(...args) {
+        if (isFinished())
+            return;
+
         context.status = "COMPLETED";
         if (args.length === 1) {
             context.exitCode = "COMPLETED";
@@ -79,6 +96,9 @@ function TaskExecution(paramsObjOrConfig) {
      * or an exit code object and a result object.
      */
     function statusFailed(...args) {
+        if (isFinished())
+            return;
+
         context.status = "FAILED";
 
         if (args.length === 1) {
@@ -94,28 +114,25 @@ function TaskExecution(paramsObjOrConfig) {
 
     // default events, can be overriden
     let events = {
-        starting: function() {
+        starting: function () {
             statusStarting();
         },
-        started: function() {
+        started: function () {
             statusStarted();
         },
-        completed: function(...args) {
+        completed: function (...args) {
             statusCompleted(...args);
         },
-        failed: function(...args) {
+        failed: function (...args) {
             statusFailed(...args);
         },
-        stopping: function() {
-            statusStoping();
+        stopping: function () {
+            statusStopping();
         },
-        stopped: function(...args) {
+        stopped: function (...args) {
             statusStopped(...args)
         }
     };
-
-    // may not need this...
-    let stopEvents = {};
 
     let subscribers = {};
 
@@ -182,6 +199,7 @@ class PageLoader extends React.Component {
         this.execution = null;
 
         this.load = this.load.bind(this);
+        this.cancel = this.cancel.bind(this);
     }
 
     load() {
@@ -213,6 +231,23 @@ class PageLoader extends React.Component {
             console.debug("HTTP " + response.status);
             execution.events.failed(response);
         });
+
+        // pretend that anything longer than 3 seconds is a timeout
+        setTimeout(() => {
+            this.execution.events.stopped("TIMEOUT");
+        }, 3000);
+    }
+
+    cancel() {
+        this.doCancel(this.execution);
+    }
+
+    doCancel(execution) {
+        execution.events.stopping();
+
+        setTimeout(function () {
+            execution.events.stopped("CANCELLED");
+        }, 1000);
     }
 
     render() {
@@ -229,14 +264,26 @@ class PageLoader extends React.Component {
                 return this.renderCompleted();
             case "FAILED":
                 return this.renderFailed();
+            case "STOPPING":
+                return this.renderCancelling();
+            case "STOPPED":
+                if (this.execution.context.exitCode === "CANCELLED")
+                    return this.renderCancelled();
+                else if (this.execution.context.exitCode === "TIMEOUT")
+                    return this.renderTimedOut();
+                else return this.renderReady();
         }
     }
 
     renderReady() {
         return (
             <div>
-                <input type="text" ref="ttl" />
-                <button className="btn btn-info" onClick={this.load}>Go</button>
+                <div className="input-group">
+                    <input type="text" className="form-control" ref="ttl" placeholder="ttl"/>
+                    <span className="input-group-btn">
+                        <button className="btn btn-info" onClick={this.load}>Go</button>
+                    </span>
+                </div>
                 <p></p>
             </div>
         )
@@ -246,6 +293,8 @@ class PageLoader extends React.Component {
         return (
             <div>
                 <button className="btn btn-info" disabled>Go</button>
+                <button className="btn btn-danger" onClick={this.cancel}>Cancel</button>
+                button>
                 <img className="showInProgress loader" src="/resources/loading.gif" alt="loading"/>
                 <p></p>
             </div>
@@ -255,7 +304,7 @@ class PageLoader extends React.Component {
     renderCompleted() {
         return (
             <div>
-                <input type="text" ref="ttl" />
+                <input type="text" ref="ttl"/>
                 <button className="btn btn-info" onClick={this.load}>Go</button>
                 <p></p>
                 <div className="alert alert-success">Done. Result: {this.execution.context.result.toString()}</div>
@@ -266,10 +315,42 @@ class PageLoader extends React.Component {
     renderFailed() {
         return (
             <div>
-                <input type="text" ref="ttl" />
+                <input type="text" ref="ttl"/>
                 <button className="btn btn-info" onClick={this.load}>Go</button>
                 <p></p>
-                <div className="showOnErrorAlert alert alert-danger">Error: {this.execution.context.result.toString()}</div>
+                <div
+                    className="showOnErrorAlert alert alert-danger">Error: {this.execution.context.result.toString()}</div>
+            </div>
+        )
+    }
+
+    renderCancelling() {
+        return (
+            <div>
+                <p></p>
+                <div className="showOnErrorAlert alert alert-danger">Attempting to cancel...</div>
+            </div>
+        )
+    }
+
+    renderCancelled() {
+        return (
+            <div>
+                <input type="text" ref="ttl"/>
+                <button className="btn btn-info" onClick={this.load}>Go</button>
+                <p></p>
+                <div className="showOnErrorAlert alert alert-danger">Canceled.</div>
+            </div>
+        )
+    }
+
+    renderTimedOut() {
+        return (
+            <div>
+                <input type="text" ref="ttl"/>
+                <button className="btn btn-info" onClick={this.load}>Go</button>
+                <p></p>
+                <div className="showOnErrorAlert alert alert-danger">Timed out.</div>
             </div>
         )
     }
